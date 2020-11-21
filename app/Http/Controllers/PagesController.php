cbb2bd6b01fbb39a113cfbeacafa349ea512b6ab
HsuBokai <exp2.718281@gmail.com>
2020-11-21 12:39:47 +0800

    title passed by parameters

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
 	public function index() {
-		return view('pages.index');
+		$title = 'Welcome To Laravel';
+		return view('pages.index')->with('title', $title);
 	}
 
 	public function about() {
-		return view('pages.about');
+		$title = 'About Us';
+		return view('pages.about')->with('title', $title);
 	}
 
 	public function services() {
