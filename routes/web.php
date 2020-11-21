a3b28028af6b49c41331a178196c419b5861b792
HsuBokai <exp2.718281@gmail.com>
2020-11-21 10:50:05 +0800

    route get with parameter
    route get multiple parameters

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// http://localhost/users?id=10&name=David
+Route::get('/users', function (Request $request) {
+    return 'This is the user '.$request->query('name').' with an id of '.$request->query('id');
+});
+
+// http://localhost/users/id
+Route::get('/users/{id}', function ($id) {
+    return 'This is the user with an id of '.$id;
+});
 
 Route::get('/', function () {
     return view('welcome');
