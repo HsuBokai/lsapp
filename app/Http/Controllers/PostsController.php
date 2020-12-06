f9f913e9661d7209c66aad69a4cb88284f29ad37
HsuBokai <exp2.718281@gmail.com>
2020-12-06 17:15:21 +0800

    edit post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -74,7 +74,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = BlogPost::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -86,7 +87,17 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $post = BlogPost::find($id);
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return redirect('posts')->with('success', 'Post Updated');
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,10 @@
 @section ('content')
 	<h1>{{$post->title}}</h1>
 	<small>Written on {{$post->created_at}}</small>
+	<hr>
 	<div>
 		{!! $post->content !!}
 	</div>
+	<hr>
+	<a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 @endsection
