ad95cdd54c33637c2d3b1b02c96507ac259913c8
HsuBokai <exp2.718281@gmail.com>
2020-12-06 16:19:41 +0800

    add create in navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,4 +6,9 @@
 		<b-nav-item href="/services">Services</b-nav-item>
 		<b-nav-item href="/posts">Blog</b-nav-item>
 	</b-navbar-nav>
+	<b-navbar-nav class="ml-auto">
+		<b-nav-item-dropdown text="Create" right>
+			<b-dropdown-item href="/posts/create">New Post</b-dropdown-item>
+		</b-nav-item-dropdown>
+	</b-navbar-nav>
 </b-navbar>
