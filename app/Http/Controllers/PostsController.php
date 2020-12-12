4a9c64649fe5569ad8ecfc0e9786c3cbec30c335
HsuBokai <exp2.718281@gmail.com>
2020-12-12 18:52:49 +0800

    delete image

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Models\BlogPost;
 //use DB;
 
@@ -151,6 +152,10 @@ class PostsController extends Controller
 
         if (auth()->user()->id !== $post->user_id) {
             return redirect('posts')->with('error', 'Unauthorized Page');
+        }
+
+        if ($post->cover_image != 'noimage.jpg') {
+            Storage::delete('public/cover_images/'.$post->cover_image);
         }
 
         $post->delete();
