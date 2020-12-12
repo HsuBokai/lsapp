2a682a95948563a4a994a62b86371dd7001bdb28
HsuBokai <exp2.718281@gmail.com>
2020-12-12 17:57:00 +0800

    add file upload button
    123

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section ('content')
 	<h1>Create Post</h1>
-	{!! Form::open(['url' => '/posts', 'method' => 'POST']) !!}
+	{!! Form::open(['url' => '/posts', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
 		<div class="form-group">
 			{{Form::label('title', 'Title')}}
 			{{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
@@ -10,6 +10,9 @@
 		<div class="form-group">
 			{{Form::label('content', 'Content')}}
 			{{Form::textarea('content', '', ['class' => 'form-control', 'placeholder' => 'Content Text'])}}
+		</div>
+		<div class="form-group">
+			{{Form::file('cover_image')}}
 		</div>
 		<b-button type="submit" variant="primary">Submit</b-button>
 	{!! Form::close() !!}
